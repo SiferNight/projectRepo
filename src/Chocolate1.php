6220d6d2ff9988154ab2758113dbe1986cdcec86
Ann <annapetukhova18@gmail.com>
2025-04-07 22:03:48 +0300

    ghgh

--- src/Chocolate1.php
+++
@@ -1,8 +0,0 @@
-<?php 
-namespace Ann\Repo;
-
-class Chocolate1 extends Snek{
-    public function __construct(string $name="Шоколадка",string $chocolate="Молочный&белый шоколад",array $toppings=["кокосовая стружка","орешки"]){
-        parent::__construct($name, $chocolate, $toppings);
-    }
-}
